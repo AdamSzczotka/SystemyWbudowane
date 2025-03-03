9e15c3e6c3c560a77fdb6698cd7a937c9522e054
Adam Szczotka <114624941+AdamSzczotka@users.noreply.github.com>
2025-03-03 11:23:16 +0000

    Zajęcia 1

---
+++ Zadnie_1/3.c
@@ -0,0 +1,26 @@
+/*
+ * Zadanie_1.c
+ *
+ * Created: 03.03.2025 11:09:54
+ * Author : student
+ */ 
+
+ #include <avr/io.h>
+ #define F_CPU 16000000UL //określamy jaki jest zegar przed biblioteką
+ #include <util/delay.h>
+ 
+ #define diodyDDR DDRA
+ #define diody PORTA
+ #define dioda 4
+ 
+ int main(void)
+ {
+     diodyDDR = 0xff;
+     diody = 0b11111111;
+     diody &= ~(1<<dioda);//0b11101111 zerowanie wybranego bitu
+     while (1) 
+     {
+     }
+ }
+ 
+ 
